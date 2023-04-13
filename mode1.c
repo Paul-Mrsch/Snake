f1a5b169359748c9eb2b1669e52fb499e67c435a
Paul Mareschi <117636519+Caalagan@users.noreply.github.com>
2023-04-14 01:34:04 +0200

    Victoire + pommes

--- mode1.c
+++ mode1.c
@@ -18,7 +18,7 @@ I : Un snake qui ne bouge pas tant que l'user n'a pas choisi ou aller
 #include <stdio.h>
 #include <stdlib.h>
 #define ROWS 20
-#define COLS 20
+#define COLS 50
 
 void mode1(){
     //variable gameover
@@ -26,7 +26,10 @@ void mode1(){
 
     //position de la tete du serpent
     int headX = 10;
-    int headY = 14;
+    int headY = 25;
+
+    //variable pour savoir si le serpent est sur l'emplacement de la pomme
+    int is_snake = 0;
 
     //variable pour savoir si la pomme est mangée
     int on_apple = 0;
@@ -35,7 +38,13 @@ void mode1(){
     int board[ROWS][COLS];
     for (int i = 0; i < ROWS; i++){
         for (int j = 0; j < COLS; j++){
-            board[i][j] = 0;
+            if (i == 0 || i == ROWS-1) {
+                board[i][j] = -1;
+            } else if (j == 0 || j == COLS-1) {
+                board[i][j] = -2;
+            } else {
+                board[i][j] = 0;
+            }
         }
     }
     
@@ -80,15 +89,14 @@ void mode1(){
         }
     }
 
-    void print_snake(Snake *snake) {
-        Node *current = snake->head;
-        while (current != NULL) {
-            printf("(%d, %d) ", current->x, current->y);
-            current = current->next;
-        }
-        printf("\n");
-    }
 
+    Snake snake = { NULL, NULL };
+    // Ajouter les premières parties du serpent à la liste
+    enqueue(&snake, 10, 23 );
+    enqueue(&snake, 10, 24 );
+    enqueue(&snake, 10, 25 );
+
+    //fonction pour faire monter le serpent
     void up_snake(Snake *snake, int row, int col){
         enqueue(snake, row-1, col);
         if (on_apple == 1){
@@ -99,6 +107,7 @@ void mode1(){
         headX--;
     }
     
+    //fonction pour faire descendre le serpent
     void down_snake(Snake *snake, int row, int col){
         enqueue(snake, row+1, col);
         if (on_apple == 1){
@@ -109,6 +118,7 @@ void mode1(){
         headX++;
     }
 
+    //fonction pour faire aller le serpent à gauche
     void left_snake(Snake *snake, int row, int col){
         enqueue(snake, row, col-1);
         if (on_apple == 1){
@@ -119,6 +129,7 @@ void mode1(){
         headY--;
     }
 
+    //fonction pour faire aller le serpent à droite
     void right_snake(Snake *snake, int row, int col){
         enqueue(snake, row, col+1);
         if (on_apple == 1){
@@ -129,18 +140,43 @@ void mode1(){
         headY++;
     }
 
-    void apple(){
-        int appleX = rand() % (ROWS-2) + 1;
-        int appleY = rand() % (COLS-2) + 1;
-        board[appleX][appleY] = 2;
+    //fonction de vérification de victoire
+    void check_win(){
+        printf("check_win \n");
+        int temp = 0;
+        for (int i = 0; i < ROWS; i++){
+            for (int j = 0; j < COLS; j++){
+                if (board[i][j] == 0){
+                    temp = 1;
+                }
+            }
+        }
+        if (temp == 0){
+            GameOver = 2;
+        }
     }
 
+    //fonction pour vérifier si le serpent est à l'emplacement
     void check_snake(Snake *snake, int row, int col){
         if (board[row][col] == 1){
+            is_snake = 1;
             GameOver = 1;
         }
     }
 
+    //fonction pour créer la pomme
+    void apple(){
+        while (1){
+            int appleX = rand() % (ROWS-2) + 1;
+            int appleY = rand() % (COLS-2) + 1;
+            if (board[appleX][appleY] == 0){
+                board[appleX][appleY] = 2;
+                break;
+            }
+        }
+    }
+    
+    //fonction pour vérifier si la pomme est à l'emplacement
     void check_apple(Snake *snake, int row, int col){
         if (board[row][col] == 2){
             on_apple = 1;
@@ -148,67 +184,101 @@ void mode1(){
         }
     }
 
+    //fonction pour vérifier si le serpent est mort
     void check_gameover(Snake *snake, int row, int col){
         if (row == 0 || row == ROWS-1 || col == 0 || col == COLS-1){
             GameOver = 1;
         }
     }
 
-    Snake snake = { NULL, NULL };
 
+    //fonction qui regroupe tous les checks
     void check(int row, int col){
-        check_snake(&snake, row, col);
-        check_apple(&snake, row, col);
+        check_win();
         check_gameover(&snake, row, col);
+        if (GameOver == 0){
+            check_snake(&snake, row, col);
+            check_apple(&snake, row, col);
+        }
+        is_snake = 0;
     }
-
-   
-
-    // Ajouter trois parties du serpent à la liste
-    enqueue(&snake, 10, 10);
-    enqueue(&snake, 10, 11);
-    enqueue(&snake, 10, 12);
-    enqueue(&snake, 10, 13);
-    enqueue(&snake, 10, 14);
 
 
     //fonction pour afficher le plateau de jeu avec un affichage d'une bordure autour
     void printBoard(char dir){
         system("cls");
-        for (int i = 0; i < ROWS; i++){
-            for (int j = 0; j < COLS; j++){
-                if (board[i][j] == 1 && i == headX && j == headY){
-                    printf("%c", dir);
-                } else if (board[i][j] == 1){
-                    printf("0");
-                } else if (board[i][j] == 2){
-                    printf("\033[1;31m");
-                    printf("X");
-                    printf("\033[0m");
-                } else if (i == 0 || i == ROWS-1){
-                    printf(".");
-                } else if (j == 0 || j == COLS-1){
-                    printf("|");
-                } else {
-                    printf(" ");
+        //mise a jour du board avec ecris win sur le tableau a la place du snake
+        if (GameOver == 2){
+            //7 pour W, 8 pour I, 9 pour N
+            for (int i = 0; i < ROWS; i++){
+                for (int j = 0; j < COLS; j++){
+                    if (board[i][j] == 1 || board[i][j] == 1 && i == headX && j == headY){
+                        if (board[i][j-1]==7){
+                            board[i][j] = 8;
+                            printf(" I ");
+                        } else if (board[i][j-1]==8){
+                            board[i][j] = 9;
+                            printf(" N ");
+                        } else if (board[i][j-1]==9){
+                            board[i][j] = 7;
+                            printf(" W ");
+                        } else {
+                            board[i][j] = 7;
+                            printf(" W ");
+                        }   
+                    } else if (board[i][j] == -1){
+                        printf("---");
+                    } else if (board[i][j] == 2){
+                        printf("\033[1;31m");
+                        printf(" X ");
+                        printf("\033[0m");
+                    } else if (board[i][j] == -2){
+                        printf("|");
+                    }
                 }
+                printf("\n");
             }
-            printf("\n");
+        } else {
+            for (int i = 0; i < ROWS; i++){
+                for (int j = 0; j < COLS; j++){
+                    if (board[i][j] == 1 && i == headX && j == headY){
+                        printf(" %c ", dir);
+                    } else if (board[i][j] == 1){
+                        printf(" 0 ");
+                    } else if (board[i][j] == 2){
+                        printf("\033[1;31m");
+                        printf(" X ");
+                        printf("\033[0m");
+                    } else if (board[i][j] == -1){
+                        printf("---");
+                    } else if (board[i][j] == -2){
+                        printf("|");
+                    } else if (board[i][j] == 0){
+                        printf("   ");
+                    }
+                }
+                printf("\n");
+            }
         }
     }
-
+    for (int i = 0; i < 857; i++){
+        apple();
+    }
+ 
     apple();
     printBoard('>');
     while (GameOver == 0){
+
+        
         //fonction pour afficher le serpent
         printf("Direction : ");
         char direction;
-        scanf("%c", &direction);
+        scanf(" %c", &direction);
         switch (direction){
             case 'z':
                 //fonction pour déplacer le serpent vers le haut
                 check(headX-1, headY);
-                if (GameOver == 1){
+                if (GameOver != 0){
                     break;
                 }
                 up_snake(&snake, headX, headY);
@@ -218,7 +288,7 @@ void mode1(){
             case 's':
                 //fonction pour déplacer le serpent vers le bas
                 check(headX+1, headY);
-                if (GameOver == 1){
+                if (GameOver != 0){
                     break;
                 }
                 down_snake(&snake, headX, headY);
@@ -228,7 +298,7 @@ void mode1(){
             case 'q':
                 //fonction pour déplacer le serpent vers la gauche
                 check(headX, headY-1);
-                if (GameOver == 1){
+                if (GameOver != 0){
                     break;
                 }
                 left_snake(&snake, headX, headY);
@@ -238,7 +308,7 @@ void mode1(){
             case 'd':
                 //fonction pour déplacer le serpent vers la droite
                 check(headX, headY+1);
-                if (GameOver == 1){
+                if (GameOver != 0){
                     break;
                 }
                 right_snake(&snake, headX, headY);
@@ -255,12 +325,22 @@ void mode1(){
         }
         direction = '0';
         on_apple = 0;
+
+        check_win();
+
         if (GameOver == 1){
             printBoard('X');
+        } else if (GameOver == 2){
+            printBoard('W');
         }
 
     }
+
     printf("\n");
-    printf("Game Over\n");    
+    if (GameOver == 1){
+        printf("Game Over\n");
+    } else {
+        printf("You Win\n");
+    }
 
 }
